b450a52cc0c324a712c043bc4bada1ab3c022952
ACBENBB <acbenbb@gmail.com>
2024-05-06 20:18:16 +0300

    FIrst commit

--- Leumit/src/PageObjects/DemoGuru99.java
+++ Leumit/src/PageObjects/DemoGuru99.java
@@ -7,9 +7,6 @@ import org.openqa.selenium.support.FindBy;
 
 public class DemoGuru99 extends BasePage {
 
-    @FindBy(xpath = "//div[@id='navbar-brand-centered']")
-    WebElement upperMenu;
-
     String headerPattern = "//div[@id='navbar-brand-centered']//a[contains(text(),'%s')]";
 
     public DemoGuru99(WebDriver driver) {
@@ -24,9 +21,12 @@ public class DemoGuru99 extends BasePage {
     public void clickUpperMenu(String mainManu, String subMenu) {
         String mainMenuXpath = String.format(headerPattern, mainManu);
         click(driver.findElement(By.xpath(mainMenuXpath)));
-        if (subMenu != null || !subMenu.equals("")){
+        try{
+        if (subMenu != null){
                 String menuXpath = String.format(headerPattern, subMenu);
                 click(driver.findElement(By.xpath(menuXpath)));
+        }}catch (NullPointerException e){
+            System.out.println("Null point exception: "+ e);
         }
     }
 }
